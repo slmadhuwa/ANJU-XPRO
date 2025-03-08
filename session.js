6e6da4136faf09976ff7d24ec29f3293f0f2f46e
Sl madhuwa <88928383+slmadhuwa@users.noreply.github.com>
2025-03-08 08:03:28 +0530

    Update session.js

--- session.js
+++ session.js
@@ -40,7 +40,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Your session id here",
-SUDO: process.env.SUDO || ["94717775628","94758775628"],
+SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~bAEFgApS#_013TiFqC-AkvPy9wvNgmezDEXTqp2wPdGOYOafoFHc",
+SUDO: process.env.SUDO || ["94770479058"],
 PORT: process.env.PORT || "8000"
 };
